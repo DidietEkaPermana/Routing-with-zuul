96c09aa38139cf0383344607792af5c7f69521a5
Didiet Eka Permana <didiet_eka_permana@hotmail.com>
2018-05-08 12:58:28 +0700

    filtering

--- books/src/main/java/hello/BookApplication.java
+++ books/src/main/java/hello/BookApplication.java
@@ -2,6 +2,9 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,14 @@ public class BookApplication {
   @RequestMapping(value = "/available")
   public String available() {
     return "{\"data\": \"Spring in Action from server 01\"}";
+  }
+  
+  @RequestMapping(value = "/check")
+  public ResponseEntity<String> check(@RequestHeader("Authorization") String token) {
+	  if(token.isEmpty())
+		  return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+	  else
+		return new ResponseEntity<String>(HttpStatus.OK);
   }
 
   @RequestMapping(value = "/checked-out")
--- gateway/src/main/java/hello/filters/pre/SimpleFilter.java
+++ gateway/src/main/java/hello/filters/pre/SimpleFilter.java
@@ -2,17 +2,31 @@ package hello.filters.pre;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.ZuulFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.core.env.Environment;
+
 
 public class SimpleFilter extends ZuulFilter {
 
   private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+
+    @Autowired
+  private Environment env;
 
   @Override
   public String filterType() {
@@ -33,39 +47,56 @@ public class SimpleFilter extends ZuulFilter {
   public Object run() {
     RequestContext ctx = RequestContext.getCurrentContext();
     HttpServletRequest request = ctx.getRequest();
+	
+	String searchString = env.getProperty("gateway.searchstring");
+	
+    String tokenFrHeader = request.getHeader(searchString);
     
-    String token = request.getHeader("Authorization");
+    String tokenFrParam = request.getParameter(searchString);
 
-    /*
-     * cookies check
-     * */
-//    Cookie[] kukis = request.getCookies();
-//   
-//    for (Cookie cookie : kukis) {
-//    	
-//    	log.info(String.format("Cookie: %s", cookie.getName()));
-//        if ("my-cookie-name".equals(cookie.getName())) {
-//             String value = cookie.getValue();
-//            //do something with the cookie's value.
-//        }
-//   }
-    
+    String tokenFrCookies = getCookieValue(request.getCookies(), searchString);
     
     /*
      * sample to call other service
      * */
     
-//    RestTemplate restTemplate = new RestTemplate();
-//    restTemplate.getForObject(url, responseType)
+    HttpHeaders headers = new HttpHeaders();
     
-    log.info(String.format("%s request to %s with token %s", request.getMethod(), request.getRequestURL().toString(), token));
+    headers.add(searchString, tokenFrParam);
+    
+    RestTemplate restTemplate = new RestTemplate();
+    
+    String url = env.getProperty("gateway.authserver.url");
+    
+    HttpEntity<String> entity = new HttpEntity<>(headers);
+	
+	int timeout = Integer.parseInt(env.getProperty("gateway.authserver.timeout"));
+	
+	((SimpleClientHttpRequestFactory)restTemplate.getRequestFactory()).setConnectTimeout(timeout);
+    
+    ResponseEntity<Object> respEntity = restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
+    
+    if(respEntity.getStatusCode() == HttpStatus.OK){
+    	log.info("get response");
+    }
+	else
+		setFailedRequest("UNAUTHORIZED", HttpStatus.UNAUTHORIZED.value());
+    
+    log.info(String.format("%s request to %s with token %s", request.getMethod(), request.getRequestURL().toString(), tokenFrHeader));
 	
     /*
      * if not appropriate send response
      * */
-	if(token.equals("nono"))
+	if(tokenFrHeader.equals("nono"))
 		setFailedRequest("UNAUTHORIZED", HttpStatus.UNAUTHORIZED.value());
 
+	
+	HttpServletResponse response = ctx.getResponse();
+	
+	Cookie cookie = new Cookie(searchString, tokenFrParam);
+	
+	response.addCookie(cookie);
+	
     return null;
   }
   
@@ -77,6 +108,16 @@ public class SimpleFilter extends ZuulFilter {
 			ctx.setResponseBody(body);
 			ctx.setSendZuulResponse(false);
 		}
+	}
+	
+	private String getCookieValue(Cookie[] kukies, String Name){
+		for (Cookie cookie : kukies){
+			if(Name.equals(cookie.getName())){
+				return cookie.getValue();
+			}
+		}
+		
+		return null;
 	}
 
 }
